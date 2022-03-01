02d309d8643182004234da358e739e456bbe5a86
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-03-01 19:05:50 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/br/com/meu/pet/MeuPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meu.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
