131b30fab5d999d5746a780fe91192110c5b3c3f
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-03-01 20:00:19 -0300

    Adiciona a pagina de login

---
+++ src/main/java/br/com/meu/pet/controller/LoginController.java
@@ -0,0 +1,19 @@
+package br.com.meu.pet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String authenticateUser(){
+        return "redirect:/home";
+    }
+}
